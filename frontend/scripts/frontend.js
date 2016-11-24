c6ccea986c9f3b21aa1a3c07fd65d6c764536671
jestersunborn <jester.js@ex.ua>
2016-11-24 18:08:51 +0200

    Add some functions.

---
+++ frontend/scripts/frontend.js
@@ -0,0 +1,24 @@
+const render = json => {
+  const wrap = document.getElementById('wrap')
+  for (item in json) {
+    const newWrap = document.createElement('ul')
+    for (field in json[item]) {
+      const newList = document.createElement('li')
+      newList.innerHTML = `${field}: ${json[item][field]}`
+      newWrap.appendChild(newList)
+    }
+    document.body.appendChild(newWrap)
+  }
+}
+
+const fetchAPI = url => {
+  fetch(`http://localhost:8080/${url}`)
+    .then(res => res.json())
+    .then(json => render(json))
+}
+
+const button = document.getElementById('button')
+
+button.onclick = _ => {
+  fetchAPI('myurl');
+}
